765e8778e11455b806ec434d5f035f8f682b7204
BKRApps <kumarreddy72@gmail.com>
2019-10-02 19:57:02 +0530

    add project setup

---
+++ iOSTesting-Techniques/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTesting-Techniques
+//
+//  Created by kumar reddy on 02/10/19.
+//  Copyright © 2019 kumar reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
